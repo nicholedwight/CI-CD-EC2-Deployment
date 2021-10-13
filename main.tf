cd9eaeb44e78d96e241beeb351447c673baeba17
Nichole Dwight <nicholedwight@gmail.com>
2021-10-13 11:03:14 -0600

    debugging and adding task definition file

--- main.tf
+++ main.tf
@@ -22,3 +22,135 @@ resource "aws_instance" "terraform_app_server" {
     Name = "ExampleTerraformAppServerInstance"
   }
 }
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "terraform_test_container"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
+
+resource "aws_ecr_repository" "terraform_ecr_repo" {
+  name                 = "terraform_project_ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecs_cluster" "terraform_ecs_cluster" {
+  name = "terraform_project_cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_service" "terraform_test_service" {
+  name            = "terraform_test_service"
+  cluster         = aws_ecs_cluster.terraform_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 3
+  iam_role        = aws_iam_role.terraform_role.arn
+  depends_on      = [aws_iam_policy.terraform_policy]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.terraform_lb_target_group.arn
+    container_name   = "terraform_test_container"
+    container_port   = 80
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
+  }
+}
+
+resource "aws_lb_target_group" "terraform_lb_target_group" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.terraform_main.id
+}
+
+resource "aws_vpc" "terraform_main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_iam_policy" "terraform_policy" {
+  name        = "terraform-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*",
+        "ecs:*",
+        "ecr:*",
+        "elasticloadbalancing:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role" "terraform_role" {
+  name = "terraform_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "test"
+        Principal = {
+          Service = ["ecs.amazonaws.com", "ecs-tasks.amazonaws.com", "ec2.amazonaws.com"]
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.terraform_role.name
+  policy_arn = aws_iam_policy.terraform_policy.arn
+}
