564d47f20c3fa428e5edda4b67aaec90b6c73bac
Nichole Dwight <nicholedwight@gmail.com>
2021-10-13 17:26:31 -0600

    more httpd work

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo mv index.html /var/www/html/
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo mv index.html /var/www/html/index.html
 sudo systemctl restart httpd
